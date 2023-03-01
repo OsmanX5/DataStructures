d3eab6becbb3d6796006f736a456b65667825e2d
Osman yousif <103122098+OsmanX5@users.noreply.github.com>
2023-03-01 07:03:18 +0200

    solva a problem

--- DataStructures/DataStructuresTest/HeapTest.cs
+++ DataStructures/DataStructuresTest/HeapTest.cs
@@ -12,34 +12,61 @@ namespace DataStructuresTest
 	{
 		static HeapTest()
 		{
-			int[] nums = { -3,4,3,3};
-			int n = nums.Length;
-			int k = 2;
-			MaxHeap<Tuple<int,int>> heap = new MaxHeap<Tuple<int, int>>(n);
+			int n = 5;
+			int[][] roads =
+			{
+				new int[] { 0, 3 },
+				new int[] { 2, 4 },
+				new int[] { 1, 3 },
+				//new int[] { 0, 2 },
+				//new int[] { 1, 3 },
+				//new int[] { 2, 4 },
+			};
+			List<int>[] Adj = new List<int>[n];
+			for (int i = 0; i < n; i++)Adj[i] = new List<int>();
+			foreach (var road in roads)
+			{
+				int a = road[0];
+				int b = road[1];
+				Adj[a].Add(b);
+				Adj[b].Add(a);
+			}
+			PrintAdj(Adj);
+			int[] importance = new int[n]; //id : score
+			MaxHeap<Tuple<int, int>> heap = new MaxHeap<Tuple<int, int>>(n);
+			for(int i=0;i<n;i++) heap.Add(new Tuple<int, int>(Adj[i].Count,i));
+			Console.WriteLine(heap.ToString());
 			for (int i = 0; i < n; i++)
 			{
-				heap.Add(new Tuple<int, int>(nums[i], i));
+				Tuple<int, int> next = heap.Pop();
+				importance[next.Item2] = n - i;
 			}
+			print(importance);
+			int res = 0;
+			foreach (var road in roads)
+			{
+				int a = road[0];
+				int b = road[1];
+				res += importance[a] + importance[b];
+			}
+			Console.WriteLine(res);
 
-			Tuple<int, int>[] res = new Tuple<int, int>[k];
-			int lastIndex = -1;
-			int resI = 0;
-			while (heap.Count() > 0)
+		}
+
+		static void PrintAdj(List<int>[] adj)
+		{
+			for (int i = 0; i < adj.Length; i++)
 			{
-				if (resI >= k) break;
-				var x = heap.Pop();
-				int index = x.Item2;
-				int val = x.Item1;
-				if (index >= lastIndex)
-				{
-					res[resI] = x;
-					resI++;
-				}
+				Console.Write($"{i} : ");
+				foreach (var VARIABLE in adj[i]) Console.Write($" {VARIABLE} ");
+				Console.WriteLine();
 			}
-			int[] res2 = (from x in res orderby x.Item2 select x.Item1).ToArray();
-			foreach (var VARIABLE in res2)
+		}
+		static void print(int[] arr)
+		{
+			for (int i = 0; i < arr.Length; i++)
 			{
-				Console.WriteLine(VARIABLE);
+				Console.Write($" [ {i }  {arr[i]} ]");
 			}
 		}
 	}
