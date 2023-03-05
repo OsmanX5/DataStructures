515787816b97425dbb1ec3c895d4cc77457c90f3
OsmanX5 <Gamemakerx5@gmail.com>
2023-03-05 07:04:51 +0200

    s

---
+++ DataStructures/DataStructures/UGraph.cs
@@ -0,0 +1,79 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndALghorithms.DataStructures
+{
+    public class UGraph
+    {
+        public Dictionary<int, HashSet<int>> adjList;
+        public int n;
+        public UGraph(int _n)
+        {
+            n = _n;
+            adjList = new Dictionary<int, HashSet<int>>();
+
+        }
+        public void AddEdges(IEnumerable<int[]> edges)
+        {
+            foreach (var edge in edges)
+            {
+                int a = edge[0];
+                int b = edge[1];
+                AddEdge(a, b);
+            }
+        }
+        public void AddEdge(int a, int b)
+        {
+            if (!adjList.ContainsKey(a))
+                adjList.Add(a, new HashSet<int>());
+            if (!adjList.ContainsKey(b))
+                adjList.Add(b, new HashSet<int>());
+            adjList[a].Add(b);
+            adjList[b].Add(a);
+        }
+        public Dictionary<int, int> ShortestDistanceFrom(int start)
+        {
+            Dictionary<int, int> Distance = new Dictionary<int, int>();
+            Dictionary<int, bool> Visited = new Dictionary<int, bool>();
+            foreach(var node in adjList.Keys)
+            {
+                Distance.Add(node, int.MaxValue);
+                Visited.Add(node, false);
+            }
+            Queue<int> BFS = new Queue<int>();
+            BFS.Enqueue(start);
+            Distance[start] = 0;
+            while(BFS.Count > 0)
+            {
+                int a = BFS.Dequeue();
+                if (Visited[a]) continue;
+                Visited[a] = true;
+                foreach (int next in adjList[a])
+                {
+                    Distance[next] = Math.Min(Distance[a]+1, Distance[next] );
+                    BFS.Enqueue(next);
+                }
+            }
+            return Distance;
+        }
+        public override string ToString()
+        {
+            string s = "";
+            foreach (var node in adjList)
+            {
+                s += node.Key.ToString() + " : ";
+                foreach (var edge in node.Value)
+                {
+                    s += edge.ToString();
+                }
+                s += "\n";
+            }
+            return s;
+        }
+
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,9 +1,48 @@
 ﻿using System;
 using DataStructures;
+using DataStructuresAndALghorithms.DataStructures;
+
 class Program
 {
 	static void Main()
 	{
-		new DataStructuresTest.UnionFindTest();
+		int[] arr = { 7};
+		arr = RemoveDublication(arr);
+
+        int n = arr.Length;
+		Dictionary<int,List<int>> NumbersPositions = new Dictionary<int,List<int>>();
+		for(int i = 0; i < n; i++)
+		{
+			if (!NumbersPositions.ContainsKey(arr[i]))
+				NumbersPositions.Add(arr[i], new List<int>());
+			NumbersPositions[arr[i]].Add(i);
+        }
+		UGraph graph = new UGraph(n);
+		for(int i = 0; i < n; i++)
+		{
+			if(i!=0) graph.AddEdge(i, i - 1);
+            if(i!=n-1)graph.AddEdge(i, i + 1);
+			
+		}
+		foreach(int val in NumbersPositions.Keys)
+		{
+			List<int> list = NumbersPositions[val];
+			for(int i=0;i < list.Count; i++)
+				for (int j = i; j < list.Count; j++)
+					graph.AddEdge(list[i], list[j]);
+		}
+		Console.WriteLine(graph.ShortestDistanceFrom(0)[n - 1]);
+	}
+
+	static int[] RemoveDublication(int[] arr)
+	{
+		List<int> arr2 = new List<int>();
+		arr2.Add(arr[0]);
+		for(int i = 1; i < arr.Length; i++)
+		{
+			if (arr[i] != arr[i - 1]) 
+				arr2.Add(arr[i]);
+		} 
+		return arr2.ToArray();
 	}
 }
